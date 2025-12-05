25e75a617c7d41b81c9a90f326de91fa855a6fe0
Cory Frontin <cory.frontin@nrel.gov>
2025-12-05 11:34:58 -0700

    wip logging with iterations

--- ard/utils/logging.py
+++ ard/utils/logging.py
@@ -6,6 +6,29 @@ import shutil
 import openmdao.core.component
 
 
+def extract_iter(component):
+    # get problem meta from the component if it's there
+    if hasattr(component, '_problem_meta'):
+        problem_meta = component._problem_meta
+    else:
+        print("DEBUG!!!!! COMPONENT DOESN'T STORE A META-PROBLEM... SO NO ITERATION NUMBER.")
+        return None  # component doesn't store a meta-problem... so no iteration number
+
+    if "model_ref" in problem_meta:
+        model = problem_meta["model_ref"]()
+    else:
+        print("DEBUG!!!!! PROBLEM DOESN'T HAVE A MODEL_REF THOUGH... SO NO ITERATION NUMBER.")
+        return None  # problem doesn't have a model_ref though... so no iteration number
+
+    if hasattr(model, "iter_count"):
+        iter_count = model.iter_count
+    else:
+        print("DEBUG!!!!! WE GOT SO CLOSE... BUT WE DIDN'T FIND AN ITER_COUNT ON MODEL.")
+        return None  # we got so close... but we didn't find an iter_count on model
+
+    print(f"DEBUG!!!!! iter_count found: {iter_count}")
+    return iter_count
+
 def name_create_log(component, iter: int = None):
     """
     For a given component, clean and create component- and rank-unique logfiles.
@@ -34,9 +57,10 @@ def name_create_log(component, iter: int = None):
             f"Expected openmdao.core.component.Component, got {type(component)}"
         )
 
-    logs_dir = ["logs"]
+    logs_dir = ["logs",]
+    iter = extract_iter(component)
     if iter is not None:
-        logs_dir += f"iter_{iter:04d}"
+        logs_dir += [f"iter_{iter:04d}"]
     subdir_logger = component.pathname.split(
         "."
     )  # mirror the comp path for a log directory
@@ -45,7 +69,7 @@ def name_create_log(component, iter: int = None):
     )  # find the reports directory
     path_logfile_template = Path(
         dir_reports.parent,
-        "logs",
+        *logs_dir,
         *subdir_logger,
         f"%s_rank{component._comm.rank:03d}.txt",
     )  # put the logs directory parallel to it
@@ -84,6 +108,7 @@ def component_log_capture(compute_func, iter: int = None):
 
     @wraps(compute_func)
     def wrapper(self, *args, **kwargs):
+
         # get log file paths
         path_stdout_log, path_stderr_log = name_create_log(self)
 
