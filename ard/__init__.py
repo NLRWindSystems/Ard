bb6e3c755e0fe898db494924c9ccd4bf49bc2e83
Cory Frontin <cory.frontin@nrel.gov>
2026-02-02 17:04:29 -0700

    update house style to actually work

--- ard/__init__.py
+++ ard/__init__.py
@@ -1,24 +1,13 @@
+from pathlib import Path
+
 from . import collection
 from . import layout
 from . import offshore
 from . import wind_query
 from . import utils
+from . import viz
 
-from pathlib import Path
+from .viz import house_style
 
-BASE_DIR = Path(__file__).absolute().parent
+BASE_DIR = Path(__file__).parent.absolute()
 ASSET_DIR = BASE_DIR / "api" / "default_systems"
-
-
-def get_house_style(use_tex=False, dark_background=True):
-    house_style_dir = BASE_DIR.parent / "assets" / "house_style"
-    ard_stylesheet = (
-        house_style_dir / f"stylesheet_ard{"" if use_tex else "_notex"}.mplstyle"
-    )
-    nrel_stylesheet = house_style_dir / f"stylesheet_nrel.mplstyle"
-    styles = []
-    if dark_background:
-        styles.append("dark_background")
-    styles.append(ard_stylesheet.as_uri())
-    styles.append(nrel_stylesheet.as_uri())
-    return styles
